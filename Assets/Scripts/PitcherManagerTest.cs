78b3f5331afafaa38adf5a16f7e293debea20e93
greg <grandersson1991@gmail.com>
2025-01-17 15:17:26 -0500

    updated pitching prototype

--- Assets/Scripts/PitcherManagerTest.cs
+++ Assets/Scripts/PitcherManagerTest.cs
@@ -14,7 +14,9 @@ public class PitcherManagerTest : MonoBehaviour
     Vector3 ballAcceleration;
     Vector3 ballJerk;
 
-    Vector3 releasePoint;
+    float totalPitchTime;
+    float deactivateBallTime;
+    float currentPitchSpeed;
 
     private bool pitchLogged = false;
     private float pitchTime = 0.0f;
@@ -24,12 +26,25 @@ public class PitcherManagerTest : MonoBehaviour
     private void Start()
     {
         PitcherTest pitcher = pitchers[0];
-        (Vector3 point, Vector3 initialVelocity, float pitchSpeed, Vector3 accel, Vector3 jerk) = pitcher.GetPitch(targetLocation);
+        (Vector3 releasePoint, Vector3 initialVelocity, float pitchSpeed, float timeToPlate, Vector3 accel, Vector3 jerk) = pitcher.GetPitch(targetLocation);
 
         Debug.Log("Init Velocity: " + initialVelocity);
         Debug.Log("Pitch Speed: " + pitchSpeed);
         Debug.Log("Accel: " + accel);
         Debug.Log("Jerk: " + jerk);
+
+        ballTransform.position = releasePoint;
+
+        currentPitchSpeed = Mathf.Abs(pitchSpeed);
+
+        Debug.Log("TimeToPlate: " + timeToPlate);
+        totalPitchTime = timeToPlate;
+
+        Debug.Log("TotalPitchTime: " + totalPitchTime);
+
+        deactivateBallTime = timeToPlate + 5 / currentPitchSpeed;
+
+        Debug.Log("DeactivateBallTime: " + deactivateBallTime);
 
         ballVelocity = initialVelocity;
         ballAcceleration = accel;
@@ -46,66 +61,25 @@ public class PitcherManagerTest : MonoBehaviour
             ballVelocity += ballAcceleration * deltaTime;
             ballTransform.position += ballVelocity * deltaTime;
 
-            if (pitchTime >= 0.5f && !pitchLogged)
+
+
+            if (pitchTime >= totalPitchTime && !pitchLogged)
             {
                 displayBall.SetActive(true);
                 displayBall.transform.position = targetLocation;
                 Debug.Log("Pitch Location Calculated At: " + pitchTime);
                 Debug.Log("Pitch Location: " + ballTransform.position);
                 Debug.Log("Pitch Velocity: " + ballVelocity);
+                Debug.Log("Pitch Acceleration: " + ballAcceleration);
                 pitchLogged = true;
-            }
-
-            if (pitchTime >= 1.0f)
-            {
                 pitchFinished = true;
             }
-        }
-    }
 
-    private IEnumerator ThrowBallCoroutine()
-    {
-        PitcherTest pitcher = pitchers[0];
-
-        while (true)
-        {
-            (Vector3 releasePoint, Vector3 initialVelocity, float pitchSpeed, Vector3 accel, Vector3 jerk) = pitcher.GetPitch(targetLocation);
-
-            Debug.Log("InitialVelocity: " + initialVelocity);
-
-            Debug.Log("Resetting ball to: " + releasePoint);
-            ballTransform.position = releasePoint;
-
-            Debug.Log("Waiting 1 second...");
-            yield return new WaitForSeconds(1.0f);
-
-            float pitchTime = Mathf.Abs(ballTransform.position.z / pitchSpeed);
-            Debug.Log("Upcoming pitch time of: " + pitchTime);
-
-            float elapsedTime = 0.0f;
-
-            ballVelocity = initialVelocity;
-
-            Debug.Log("Entering Movement Loop");
-            while (elapsedTime <= pitchTime)
+            if (pitchTime >= deactivateBallTime)
             {
-                float deltaTime = Time.deltaTime;
-                elapsedTime += deltaTime;
-
-                Debug.Log("Moving ball");
-                ballAcceleration += jerk * deltaTime;
-                ballVelocity += ballAcceleration * deltaTime;
-                ballTransform.position += ballVelocity * deltaTime;
-                yield return null;
+                pitchFinished = true;
+                Debug.Log("Pitch finished.");
             }
-
-            Debug.Log("Resetting Ball");
-            ballJerk = Vector3.zero;
-            ballAcceleration = Vector3.zero;
-            ballVelocity = Vector3.zero;
-
-            yield return new WaitForSeconds(1.0f);
         }
     }
-    
 }
--- Assets/Scripts/SpecificPitchTest.cs
+++ Assets/Scripts/SpecificPitchTest.cs
@@ -7,15 +7,12 @@ public class SpecificPitchTest
     [SerializeField] private float pitchSpeed; // m/s
     [SerializeField] private float horizontalBreak; // m/s
     [SerializeField] private float verticalBreak; // m/s --- does not include gravity
-    [SerializeField] private float horizontalJerk;
-    [SerializeField] private float verticalJerk;
-
+    [SerializeField] private float sharpBreakPct; // % of acceleration increase by end of pitch
+    [SerializeField] private float customGravity = -9.81f;
+    [SerializeField] private float customDragPct = -0.1f;
     // ADD MIN AND MAX RANGES FOR SPEED AND BREAK LATER
 
-    private const float dragPct = -0.1f;
-    private Vector3 gravity = new(0, -9.81f, 0);
-
-    public (Vector3, float, Vector3, Vector3) GetPitch(Vector3 releasePoint, Vector3 targetLocation)
+    public (Vector3, float, float, Vector3, Vector3) GetPitch(Vector3 releasePoint, Vector3 targetLocation)
     {
         float distanceToPlate = Mathf.Abs(releasePoint.z - targetLocation.z);
 
@@ -36,18 +33,29 @@ public class SpecificPitchTest
         float v0z = (dz - 0.5f * accel.z * t2 - (1f / 6f) * jerk.z * t3) / timeToPlate;
 
         Vector3 initialVelocity = new(v0x, v0y, v0z);
-
-        return (initialVelocity, pitchSpeed, accel, jerk);
+        return (initialVelocity, pitchSpeed, timeToPlate, accel, jerk);
     }
 
     private (float, Vector3, Vector3) GetInitialCalculations(float distanceToPlate)
     {
         if (pitchSpeed == 0) Debug.LogError("Pitch Speed has not been set.");
 
+        // ensure negativity
+        pitchSpeed = -Mathf.Abs(pitchSpeed);
+        customGravity = -Mathf.Abs(customGravity);
+        customDragPct = -Mathf.Abs(customDragPct);
+
+        // ensure positive time to plate
         float timeToPlate = Mathf.Abs(distanceToPlate / pitchSpeed);
-        float dragJerk = (dragPct * pitchSpeed) * 2f / (timeToPlate * timeToPlate);
+        float t2 = timeToPlate * timeToPlate;
+        float t3 = t2 * timeToPlate;
+
+        float dragJerk = (customDragPct * pitchSpeed) * 2f / (t2);
+        float horizontalJerk = (sharpBreakPct * horizontalBreak) * 6f / t3;
+        float verticalJerk = (sharpBreakPct * verticalBreak) * 6f / t3;
 
         Vector3 accel = new(horizontalBreak, verticalBreak, 0);
+        Vector3 gravity = new(0, customGravity, 0);
         accel += gravity;
         Vector3 jerk = new(horizontalJerk, verticalJerk, dragJerk);
 
